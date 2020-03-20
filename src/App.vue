d4b6cb2c60be31706abb2f1a897b149ed1e45063
JayKay <jaykay91.me@gmail.com>
2020-03-20 21:01:15 +0900

    vue init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello, Chat-App!!"
+    };
+  }
+};
+</script>
+
+<style>
+h1 {
+  color: green;
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+
+module.exports = {
+  devtool: "eval-source-map",
+  mode: "development",
+  resolve: {
+    extensions: [".vue", ".js"]
+  },
+  entry: {
+    app: "./src/main.js"
+  },
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "static/dist"),
+    publicPath: "dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: "vue-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [new VueLoaderPlugin()]
+};
